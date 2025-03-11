54ad6705ed607e02b9e34eb603d73ef1d5ccd2b6
Changbeom Choi <me@cbchoi.info>
2025-03-11 16:31:40 +0900

    second

---
+++ CBCHOI.java
@@ -0,0 +1,18 @@
+
+/*
+public class Hello {
+
+	public static void main(String[] ars) 
+	{
+		System.out.println("Hello, World!");
+	} 
+}
+*/
+
+public class CBCHOI {
+
+	public static void main(String[] ars) 
+	{
+		System.out.println("20211001 CBCHOI");
+	} 
+}
